39caf357a4ffda41c643a9e1ae4a3f42f60f2465
Clayton0205 <claytonadonis5@gmail.com>
2022-06-15 16:01:15 +0200

    ageToDays

---
+++ script/script.js
@@ -0,0 +1,9 @@
+const calculate = document.getElementById('calculate');
+
+calculate.addEventListener('click',()=>{
+    let age = document.getElementById('Age').value;
+    let total = (age * 365)
+    document.getElementById('display').innerHTML =
+    total + " days" ;
+}
+);
